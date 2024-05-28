cf7a6181e47493d0a58c16382494718ab73bda67
Jhonattan Amagua <86901758+JhonattanStevenAmaguaTaco@users.noreply.github.com>
2024-05-28 10:45:57 -0500

    Update news.php
    Add create function

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -30,5 +30,24 @@
 			$methodName="view";
 			loadDefaultNewsView($data,$methodName);
 		}
+		public function create()
+		{
+			$this->load->helper("form");
+			$this->load->library("form_validation");
+			$data["title"] = "Create a news item"
+			$this->form_validation->set_rules("title","Title","required");
+			$this->form_validation->set_rules("text","text","required");
+			if ($this->form_validation->run()===FASLE)
+			{
+				$this->load->view('templates/header', $data);	
+				$this->load->view('news/create');
+				$this->load->view('templates/footer');
+			}
+			else
+			{
+				$this->news_model->set_news();
+				$this->load->view('news/success');
+			}
+		}
 	}
 ?>
